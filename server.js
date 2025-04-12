b57f34c61c4e0d65a402c9201896578dfd3a16b5
BrezzeT <veteroktm809@gmail.com>
2025-04-12 16:46:45 +0300

    Update CORS configuration

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://fq-fmbr1baev-brezzets-projects.vercel.app', 'http://localhost:3000'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
